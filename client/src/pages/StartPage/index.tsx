894f1315d5f89a209bd49fc7333dcc690815c0ca
GavristovDenis <gavristov@inbox.ru>
2022-08-06 00:29:32 +0300

    Start Page development start

--- client/src/pages/StartPage/index.tsx
+++ client/src/pages/StartPage/index.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import './startPage.scss';
+
 export const StartPage = () => {
   return (
     <div>
-      <header className='header'>ЛинкедИн для пацанчиков</header>
-      <body className='body'>sdajfnsjkfg</body>
+      <header className='header'>
+        <span className='logo'>ЛинкедИн для пацанов</span>
+        <span className='headerStick'></span>
+        <span className='logSign'>Войти/Регистрация</span>
+      </header>
+      <body className='body'>
+        <span>Найдите вакансии от лучших компаний</span>
+        <input className='searchBar' placeholder='Найти Вакансию'></input>
+      </body>
     </div>
   );
 };
