45ca4bbdf730253645e79ad63c6f43f01deedcaa
GavristovDenis <gavristov@inbox.ru>
2022-08-06 00:16:51 +0300

    first commit

--- client/src/pages/StartPage/index.tsx
+++ client/src/pages/StartPage/index.tsx
@@ -1,4 +1,9 @@
 import React from 'react';
 export const StartPage = () => {
-  return <div>Start Page</div>;
+  return (
+    <div>
+      <header className='header'>ЛинкедИн для пацанчиков</header>
+      <body className='body'>sdajfnsjkfg</body>
+    </div>
+  );
 };
