f4a8819c2e89f98afc7e497cdaec4adf8d5fd1e7
GavristovDenis <gavristov@inbox.ru>
2022-08-07 19:30:39 +0300

    Start Page raw

--- client/src/pages/StartPage/index.tsx
+++ client/src/pages/StartPage/index.tsx
@@ -10,8 +10,27 @@ export const StartPage = () => {
         <span className='logSign'>Войти/Регистрация</span>
       </header>
       <body className='body'>
-        <span>Найдите вакансии от лучших компаний</span>
-        <input className='searchBar' placeholder='Найти Вакансию'></input>
+        <div className='firstPage'>
+          <span>Найдите вакансии от лучших компаний</span>
+          {''}
+          <input className='searchBar' placeholder='Найти Вакансию'></input>
+        </div>
+        <div className='secondPage'>
+          <span>Размести свою вакансию и найди специалистов</span>
+          {''}
+          <span className='secondPageStick'></span>
+          {''}
+          <button className='submitVacationButton'>Разместить вакансию</button>
+        </div>
+        <div className='thirdPage'>
+          <span>Начни общаться с коллегами</span>
+          <span className='verticalStick'></span>
+          <span>
+            “Сайт нейм” позволяет общаться с единомышленниками и обмениваться
+            опытом
+          </span>
+        </div>
+        <footer>Данный сайт был сделан только ради денег</footer>
       </body>
     </div>
   );
