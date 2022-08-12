f3e9462beba3974235bbe0b9f43bb7df84fb8212
GavristovDenis <gavristov@inbox.ru>
2022-08-12 16:08:33 +0300

    Start Page ver 2 (react router dom installed)

--- client/src/pages/StartPage/index.tsx
+++ client/src/pages/StartPage/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './startPage.scss';
+
 import Header from './components/Header';
 import { Body } from './components/Body';
 import { Footer } from './components/Footer';
