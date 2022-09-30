fa35758d83833fa5b0aa972bb7315dc876977029
GavristovDenis <102148379+GavristovDenis@users.noreply.github.com>
2022-09-30 14:04:04 +0300

    Article ver 2 (Clickable Hearts + likeCount)

--- client/src/pages/Article/components/Article_placeholder.tsx
+++ client/src/pages/Article/components/Article_placeholder.tsx
@@ -1,19 +1,50 @@
 import React from 'react';
+import { RequireAuth } from '../../../hoc/RequireAuth';
 import './Article_footer.scss';
+import '../Article_placeholder.scss';
+import { useState } from 'react';
+
 export const ArcticlePlaceholder = ({
   children,
   title,
   style,
   author,
-  ...props
+  likes,
+  views,
+  text,
 }: any) => {
+  const [isLiked, setIsLiked] = useState<boolean>(true);
+  const [likeCount, setLikeCount] = useState<number>(likes);
+  const likeClick = () => {
+    return setIsLiked(false), setLikeCount(likeCount + 1);
+  };
+  const likeClickToInactive = () => {
+    return setIsLiked(true), setLikeCount(likeCount - 1);
+  };
   return (
-    <div className={style}>
+    <div className='article_placeholder__basic'>
       <div>
         <span>{title}</span>
       </div>
+      <div>
+        <span className='article_content'>{text}</span>
+      </div>
       <div className='article_footer'>
         <span className='article_footer_text'>Автор: {author}</span>
+        <div className='article_footer_views_and_likes_container'>
+          <span className='article_footer_like_count'>
+            <span
+              className={isLiked ? 'heart_inactive' : 'heart_active'}
+              onClick={
+                isLiked ? () => likeClick() : () => likeClickToInactive()
+              }
+            >
+              &hearts;
+            </span>{' '}
+            Лайки: {likeCount}
+          </span>
+          <span>Просмотры: {views}</span>
+        </div>
       </div>
     </div>
   );
--- client/src/pages/Article/index.tsx
+++ client/src/pages/Article/index.tsx
@@ -1,25 +1,47 @@
 import React from 'react';
 import { ArcticlePlaceholder } from './components/Article_placeholder';
 import './Article_placeholder.scss';
+import './index.scss';
 import { ArticleHeader } from './components/Header';
+import { useState } from 'react';
+
+const dataForArticle: any = [
+  {
+    title: 'First title',
+    description: '30',
+    image: '30',
+    likes: 30,
+  },
+  {
+    title: '30',
+    description: '30',
+    image: '30',
+    likes: 30,
+  },
+  {
+    title: '30',
+    description: '30',
+    image: '30',
+    likes: 30,
+  },
+];
+
 export const Article = () => {
   return (
-    <div>
+    <div className='article_body'>
       <ArticleHeader />
       <div className='article_placeholder'>
-        <ArcticlePlaceholder
-          title='Article 1'
-          style='article_placeholder__basic'
-          author='Denis'
-        />
-        <ArcticlePlaceholder
-          title='Article 2'
-          style='article_placeholder__basic'
-        />
-        <ArcticlePlaceholder
-          title='Article 3'
-          style='article_placeholder__basic'
-        />
+        {dataForArticle.map((item: any, index: any) => {
+          return (
+            <ArcticlePlaceholder
+              key={index}
+              title={item.title}
+              description={item.description}
+              image={item.image}
+              likes={item.likes}
+            />
+          );
+        })}
       </div>
     </div>
   );
