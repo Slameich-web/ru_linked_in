fadf09a7b3131ea589c742bcfe6f88a4fb311dfc
Slameich-web <58104415+Slameich-web@users.noreply.github.com>
2022-10-29 01:15:03 +0300

    Profile page

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -7,7 +7,7 @@ export const Layout = () => {
       <header>Header</header>
       <div className="layout_wrapper_main">
         <nav className="layout_wrapper_nav">
-          <CustomLink to={'/profile_page'}>profile_page</CustomLink>
+          <CustomLink to={'/profile_page/qwe'}>profile_page</CustomLink>
           <CustomLink to={'/recover_password'}>recover_password</CustomLink>
           <CustomLink to={'/authorization'}>authorization</CustomLink>
         </nav>
--- client/src/pages/ProfilePage/index.tsx
+++ client/src/pages/ProfilePage/index.tsx
@@ -1,6 +1,29 @@
 import { useParams } from 'react-router-dom';
-
+import './Profile.scss';
 export const ProfilePage = () => {
   const { id } = useParams();
-  return id ? <div>ProfilePage user {id}</div> : <div>ProfilePage</div>;
+  console.log(id);
+  return (
+    <div className="profile_wrapper">
+      <div>
+        <div className="profile_wrapper_photo">
+          <img
+            src="https://avatars.mds.yandex.net/i?id=3e9d902caf2729486cf5949bd36eaa49-4569757-images-thumbs&n=13"
+            alt="Photo"
+          />
+        </div>
+        <div className="profile_wrapper_under_photo">
+          <div>Локация:</div> <div>Moscow</div>
+          <div>Телефон:</div> <div>89031147335</div>
+          <div>Telegram:</div> <div>@Slameich</div>
+        </div>
+      </div>
+      <div className="profile_wrapper_about">
+        <div>Prokhorov Aleksandr</div>
+        <div>Мои навыки: React Typescript Zustand</div>
+        <div>Про слов про меня: Я разработчик</div>
+        <div>Место работы: MTS BANK</div>
+      </div>
+    </div>
+  );
 };
